a0b74748f4f6a72652108dbd8f423d628a81d972
mthskzk <116724950+mthskzk@users.noreply.github.com>
2023-01-11 08:33:12 +0000

    [追加]投稿機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,33 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.customer_id = current_customer.id
+    # tag_list = params[:post][:tag_name].split(',') ←タグ機能のやつ
+    if @post.save
+      # @post.save_tags(tag_list)
+      redirect_to posts_path, notice: "投稿しました"
+    else
+      render :new
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, images: [])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,13 @@
 class Post < ApplicationRecord
 
   belongs_to :customer
-  belongs_to :cafe
+  belongs_to :cafe, optional: true
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :book_tags
+
+  # 複数枚の画像を取り込み
+  has_many_attached :images
 
 end

5bfd15ee86868e78f4c00fda77440c1adcdc7e48
mthskzk <116724950+mthskzk@users.noreply.github.com>
2023-02-10 07:30:33 +0000

    投稿機能修正 カフェ名に選択ボックス追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -19,14 +19,23 @@ class Public::PostsController < ApplicationController
     @post.customer_id = current_customer.id
     # gsubで全角スペースを半角スペースに変換しgsubで半角スペースを削除
     tag_list = params[:post][:tag_name].gsub(/　/," ").gsub(" ","").split(',')
-    cafe_name = params[:post][:cafe_name]
-    cafe_exist = Cafe.find_by(name: cafe_name)
-    if cafe_exist.nil?
-      cafe = Cafe.new(name: cafe_name, prefectures: "", address_after: "", nearest_station: "", telephone_number: "", business_hours: "", regular_holiday: "")
-      cafe.save
-      @post.cafe_id = cafe.id
-    else
-      @post.cafe_id = cafe_exist.id
+    # 既存のカフェを選択して投稿された場合
+    if params[:post][:cafe_number] == "1"
+      @post.cafe_id = params[:post][:cafe_id]
+    # 新しいカフェを選択して投稿された場合
+    elsif params[:post][:cafe_number] == "2"
+      cafe_name = params[:post][:cafe_name]
+      # フォームに入力されたカフェ名が既にあるか確認
+      cafe_exist = Cafe.find_by(name: cafe_name)
+      # 未登録のカフェの場合
+      if cafe_exist.nil?
+        cafe = Cafe.new(name: cafe_name, prefectures: "", address_after: "", nearest_station: "", telephone_number: "", business_hours: "", regular_holiday: "")
+        cafe.save
+        @post.cafe_id = cafe.id
+      # 登録済みカフェの場合
+      else
+        @post.cafe_id = cafe_exist.id
+      end
     end
     if @post.save
       @post.save_tags(tag_list)
